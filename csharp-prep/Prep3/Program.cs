1d5e666ae21e420a6328ef762d7bc5e55f11c99d
Cindy Mazza <maz22004@byui.edu>
2023-04-19 14:15:17 -0400

    created magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();  
+        
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            int magicNumber = randomGenerator.Next(1,101);
+            
+            int guessCount = 0;
+
+        
+            int userGuess = -1;
+        
+            while (userGuess != magicNumber)
+            {   
+                guessCount = guessCount +1;
+
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Guess lower next time.");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Guess higher next time.");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} guesses!");
+                }
+            }
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }    
     }
 }
