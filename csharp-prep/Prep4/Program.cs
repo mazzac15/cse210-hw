5b3c5f5c13278c84a36d13c040a5de48797af67b
Cindy Mazza <maz22004@byui.edu>
2023-04-20 12:33:26 -0400

    created number list program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number (0 to quit): ");
+
+            string userAnswer = Console.ReadLine();
+            userNumber = int.Parse(userAnswer);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+
+        }
+
+    //compute sum
+
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+
+    Console.WriteLine($"The sum is: {sum}");
+
+    //compute average
+
+    float average = ((float)sum) / numbers.Count;
+    Console.WriteLine($"The average is: {average}");
+    
+    //find max
+    int max = numbers[0];
+
+    foreach (int number in numbers)
+    {
+        if (number > max)
+        {
+            max = number;
+        }
+    }    
+
+    Console.WriteLine($"The max is: {max}");
     }
 }
