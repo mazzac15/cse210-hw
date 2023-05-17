dca9b12167f7724ccfcf15e96dc9e925e6204514
Cindy Mazza <maz22004@byui.edu>
2023-05-17 16:03:02 -0400

    completed learning week5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square shape1 = new Square("Green", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Red", 3, 5);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Blue", 5);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
+
+
+
+
+
+
+
+ 
+        
+
+    }
+
+    public static void DisplayShapes(Shape shape)
+    {
+        
     }
 }
